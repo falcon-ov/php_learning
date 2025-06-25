105b19076d55befe4fa3ceaaa54b82ff2211cd12
falcon-ov <vinifon4he@gmail.com>
2025-06-25 12:07:46 +0300

    update_to_problem_2

--- solved_problems/problem_2.php
+++ solved_problems/problem_2.php
@@ -32,16 +32,12 @@ function wrapTextByLength($input, $lenght)
             }
         }
     }
-    $output = implode('', $lines);
+    $output = implode(PHP_EOL, $lines);
     return $output;
 }
 
 $input =
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, 
-quia error dicta quidem illo debitis soluta non atque nihil 
-reprehenderit distinctio blanditiis in unde cupiditate reiciendis 
-magni cumque vero dolorem neque velit? Debitis rem in earum adipisci, 
-praesentium soluta illo dignissimos! Modi neque veritatis recusandae 
-est fugiat alias! Iusto, architecto.";
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, quia error dicta quidem illo debitis soluta non atque nihil reprehenderit distinctio blanditiis in unde cupiditate reiciendis magni cumque vero dolorem neque velit? Debitis rem in earum adipisci, praesentium soluta illo dignissimos! Modi neque veritatis recusandae est fugiat alias! Iusto, architecto.";
+
 
 echo wrapTextByLength($input, 70);
