417c5d4b92a28e043db72be75e8b9557c3143aa1
falcon-ov <vinifon4he@gmail.com>
2025-06-26 21:42:52 +0300

    update

--- app/Worker.php
+++ app/Worker.php
@@ -3,6 +3,55 @@
 namespace App;
 
 class Worker{
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getAge(): int
+    {
+        return $this->age;
+    }
+
+    public function setAge(int $age): void
+    {
+        $this->age = $age;
+    }
+
+    public function getHours(): array
+    {
+        return $this->hours;
+    }
+
+    public function setHours(array $hours): void
+    {
+        $this->hours = $hours;
+    }
+
+    public function getPosition(): string
+    {
+        return $this->position;
+    }
+
+    public function setPosition(string $position): void
+    {
+        $this->position = $position;
+    }
+
+    public function getExperience(): int
+    {
+        return $this->experience;
+    }
+
+    public function setExperience(int $experience): void
+    {
+        $this->experience = $experience;
+    }
 
     private string $name;
     private int $age;
@@ -12,23 +61,20 @@ class Worker{
 
     private int $experience;
 
-    public function __construct($name, $age, $hours, $position)
+    /**
+     * @param string $name
+     * @param int $age
+     * @param array $hours
+     */
+    public function __construct(string $name, int $age, array $hours)
     {
         $this->name = $name;
         $this->age = $age;
         $this->hours = $hours;
-        $this->position = $position;
     }
 
     public function work(){
         print_r("Im woriking!");
     }
 
-    public function setPosition($value){
-        $this->position = $value;
-    }
-
-    public function getPosition(){
-        return $this->position;
-    }
 }
