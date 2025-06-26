d7e368e107f4830a264646807c5ca6973b8e05ec
falcon-ov <vinifon4he@gmail.com>
2025-06-26 19:14:02 +0300

    update

---
+++ app/Developer.php
@@ -0,0 +1,10 @@
+<?php 
+
+namespace App;
+
+class Developer extends Worker{
+    public function work()
+    {
+        echo "Im developing";
+    }
+}
--- app/Worker.php
+++ app/Worker.php
@@ -1,12 +1,34 @@
 <?php
 
+namespace App;
+
 class Worker{
 
-    public string $name;
-    public int $age;
-    public array $hours;
+    private string $name;
+    private int $age;
+    private array $hours;
+
+    private string $position;
+
+    private int $experience;
+
+    public function __construct($name, $age, $hours, $position)
+    {
+        $this->name = $name;
+        $this->age = $age;
+        $this->hours = $hours;
+        $this->position = $position;
+    }
 
     public function work(){
         print_r("Im woriking!");
+    }
+
+    public function setPosition($value){
+        $this->position = $value;
+    }
+
+    public function getPosition(){
+        return $this->position;
     }
 }
